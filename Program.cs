80586cfca11477b629c449e22c684993beb78cbb
Laurits Bentsen <laurits2@hotmail.com>
2023-02-27 14:35:20 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[,] grades = new int[2, 5];
+        int sumGroupOne = 0;
+        int sumGroupTwo = 0;
+
+        Console.WriteLine("Input grades into array");
+        for (int row = 0; row < grades.GetLength(0); row++)
+        {
+            for (int column = 0; column < grades.GetLength(1); column++)
+            {
+                Console.Write($"Group {row + 1}, student {column + 1}: ");
+                grades[row, column] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+   
+        for (int row = 0; row < grades.GetLength(0); row++)
+        {
+            for (int column = 0; column < grades.GetLength(1); column++)
+            {
+                if (row < grades.GetLength(0) - 1)
+                {
+                    sumGroupOne += grades[row, column];
+                }
+                else
+                {
+                    sumGroupTwo += grades[row, column];
+                }
+            }
+        }
+
+        Console.WriteLine($"The average of group 1 is: {sumGroupOne / grades.GetLength(1)}.");
+        Console.WriteLine($"The average of group 2 is: {sumGroupTwo / grades.GetLength(1)}.");
     }
 }
 
